99a3f4266e3226bc231b2d645e0603d9cdd4c20c
lasha <maisuradze.llasha@gmail.com>
2025-04-23 20:54:24 +0400

    lil work done

--- HwAng/hw/src/app/app.component.ts
+++ HwAng/hw/src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, HomeComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
