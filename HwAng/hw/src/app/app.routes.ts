384e258b2e072b41ed93d925d6a5d58e1269a290
lasha <maisuradze.llasha@gmail.com>
2025-04-25 00:03:22 +0400

    some work done

--- HwAng/hw/src/app/app.routes.ts
+++ HwAng/hw/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    
+    {path: 'user', component: UserComponent}
 ];
 
--- HwAng/hw/src/app/user/user.component.ts
+++ HwAng/hw/src/app/user/user.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { Student } from '../Models/student'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
-
+  user = new Student()
+  userArr :any[] = []
+  
+  addStudent(){
+    this.userArr.push(this.user)
+    console.log(this.userArr)
+  }
 }
