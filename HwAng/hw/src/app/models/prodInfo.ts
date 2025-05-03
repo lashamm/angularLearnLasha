a6908284569dfe4f3a013fbf5df3cdc7c478d025
lasha <maisuradze.llasha@gmail.com>
2025-05-03 18:28:26 +0400

    done and better

--- HwAng/hw/src/app/models/prodInfo.ts
+++ HwAng/hw/src/app/models/prodInfo.ts
@@ -1,26 +1,26 @@
 export class productsInfo{
    id!:number;
-   title!:string;
-   description!:string;
-   category!:string;
-   price!:number;
-   discountPercentage!:number;
-   rating!:number;
-   stock!:number;
-   tags!: string[];
-   brand!:string;
-   sku!:string;
-   weight!:number;
-   dimensions!:dimensions;
-   warrantyInformation!:string;
-   shippingInformation!:string;
-   availabilityStatus!:string;
-   reviews!:review[];
-   returnPolicy!:string;
-   minimumOrderQuantity!: number;
-   meta!:meta;
-   images!:string[];
-   thumbnail!: string;
+   title?:string;
+   description?:string;
+   category?:string;
+   price?:number;
+   discountPercentage?:number;
+   rating?:number;
+   stock?:number;
+   tags?: string[];
+   brand?:string;
+   sku?:string;
+   weight?:number;
+   dimensions?:dimensions;
+   warrantyInformation?:string;
+   shippingInformation?:string;
+   availabilityStatus?:string;
+   reviews?:review[];
+   returnPolicy?:string;
+   minimumOrderQuantity?: number;
+   meta?:meta;
+   images?:string[];
+   thumbnail?: string;
 }
 
 class dimensions{
