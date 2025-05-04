1e236e859ba3f3cb7bf8c8636984e540b68298e9
lasha <maisuradze.llasha@gmail.com>
2025-05-04 20:20:10 +0400

    half done

--- HwAng/hw/src/app/models/prodInfo.ts
+++ HwAng/hw/src/app/models/prodInfo.ts
@@ -1,51 +1,14 @@
 export class productsInfo{
-   id!:number;
-   title?:string;
-   description?:string;
-   category?:string;
-   price?:number;
-   discountPercentage?:number;
-   rating?:number;
-   stock?:number;
-   tags?: string[];
-   brand?:string;
-   sku?:string;
-   weight?:number;
-   dimensions?:dimensions;
-   warrantyInformation?:string;
-   shippingInformation?:string;
-   availabilityStatus?:string;
-   reviews?:review[];
-   returnPolicy?:string;
-   minimumOrderQuantity?: number;
-   meta?:meta;
-   images?:string[];
-   thumbnail?: string;
+id!:number;
+title?:string;
+price?:number;
+description?:string;
+category?:string;
+image?:string;
+rating?:rating;
 }
 
-class dimensions{
-    width!:number;
-    height!:number;
-    depth!:number;
-}
-
-class meta{
-    createdAt!:string;
-    updatedAt!:string;
-    barcode!:string;
-    qrCode!:string;
-}
-
-class review{
-    rating!:number;
-    comment!:string;
-    date!:string;
-    reviewerName!:string;
-    reviewerEmail!:string;
-}
-
-
-
-export class products {
-    products!: productsInfo[];
+class rating{
+    rate?:number;
+    count?:number;
 }
